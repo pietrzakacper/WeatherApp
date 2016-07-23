d09624e028c2b633ff6c86ac10e51f7563b31951
Kacper Pietrzak <kacpietrzak@gmail.com>
2016-07-23 19:39:41 +0200

    Finishing commit

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
-function setWeather(obj){
-  $("#city").html(obj.name+", "+obj.sys.country);
-  $("#description").html(obj.weather[0]["description"]);
-  $("#temp").html(obj.main.temp+"&#x2103");
-  $("#image").attr("src",'http://openweathermap.org/img/w/'+obj.weather[0].icon+'.png');
-  $("#min-temp").html(obj.main.temp_min);
-  $("#max-temp").html(obj.main.temp_max);
-  $("#wind").html(obj.wind.speed);
-  $("#pres").html(obj.main.pressure);
-  $("#lat").html(obj.coord.lat);
-  $("#lon").html(obj.coord.lon);
+function setWeather(obj) {
+    $("#city").html(obj.name + ", " + obj.sys.country);
+    $("#description").html(obj.weather[0]["description"]);
+    $("#temp").html(obj.main.temp);
+    $("#image").attr("src", 'http://openweathermap.org/img/w/' + obj.weather[0].icon + '.png');
+    $("#min-temp").html(obj.main.temp_min);
+    $("#max-temp").html(obj.main.temp_max);
+    $("#wind").html(obj.wind.speed);
+    $("#pres").html(obj.main.pressure);
+    $("#lat").html(obj.coord.lat);
+    $("#lon").html(obj.coord.lon);
 
 }
 
-function getWeather(latitude,longitude) {
+function getWeather(latitude, longitude) {
     $.ajax({
         dataType: 'jsonp',
         data: {
-          appid: "17cc90635d35d48f549bc86c972d8afc",
-          lat: latitude,
-          lon: longitude,
-          units: "metric"
+            appid: "17cc90635d35d48f549bc86c972d8afc",
+            lat: latitude,
+            lon: longitude,
+            units: "metric"
         },
         url: 'http://api.openweathermap.org/data/2.5/weather',
         jsonpCallback: "setWeather",
         error: function() {
             console.log("Coś poszło nie tak przy losowaniu:(");
         },
-        success: function(){
-          console.log("Pomyślnie połączono z bazą lat: "+latitude+", long: "+longitude);
+        success: function() {
+            console.log("Pomyślnie połączono z bazą lat: " + latitude + ", long: " + longitude);
         }
     });
 }
 
-$(document).ready(function(){
-  if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(function(position) {
-getWeather(position.coords.latitude,position.coords.longitude);
-    })
-  }
+function switchUnitSystem() {
+    $(".fahr, .cels").each(
+        function() {
+            if ($(this).hasClass("fahr")) {
+                $(this).removeClass("fahr");
+                $(this).addClass("cels");
+                $(this).html("&#x2103;");
+            } else {
+
+                $(this).removeClass("cels");
+                $(this).addClass("fahr");
+                $(this).html("&#x2109;");
+            }
+        });
+
+    var $temp = $("#temp");
+    var $mintemp = $("#min-temp");
+    var $maxtemp = $("#max-temp");
+    if ($temp.html() < 60) {
+      $temp = toFahr($temp);
+      $mintemp = toFahr($mintemp);
+      $maxtemp = toFahr($maxtemp);
+    }  else{
+      $temp = toCels($temp);
+      $mintemp = toCels($mintemp);
+      $maxtemp = toCels($maxtemp);
+    }
+
 }
-);
+
+function toFahr($obj){
+  $obj.html(Math.round((32 + (9/5)*$obj.html())*100)/100);
+  return $obj;
+}
+
+function toCels($obj){
+  $obj.html(Math.round((($obj.html() -32)*(5/9))*100)/100);
+  return $obj;
+}
+
+$(document).ready(function() {
+    $("#switch").on("click", function() {
+        switchUnitSystem();
+    });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            getWeather(position.coords.latitude, position.coords.longitude);
+        })
+    }
+});
